dda0fbeca005d80116575f3655644cc2544dba97
technologicza <jean.louw@technologic.za.net>
2021-08-04 09:58:37 +0200

    Add files via upload

---
+++ Remove-GroupSIDHistory.ps1
@@ -0,0 +1,22 @@
+﻿$GroupOU = 'OU=ouname,DC=domain,DC=com'
+$AllGroups = Get-ADGroup -SearchBase $GroupOU -Filter *
+
+
+foreach ($Group in $AllGroups){
+
+$GroupValue = $Group.Name
+
+$SIDValue = (Get-ADGroup -Identity $GroupValue -Properties SidHistory | Select-Object -ExpandProperty SIDHistory).Value
+
+if ($SIDValue -ne $null){
+Write-Host "..removing" $SIDValue "from" $GroupValue -ForegroundColor Red
+#Set-ADGroup -Identity $GroupValue -Remove @{SIDHistory=$SIDValue}
+}
+
+else{
+Write-Host "..skipping" $GroupValue "as it has no SIDHistory" -ForegroundColor Green
+}
+
+Remove-Variable GroupValue
+Remove-Variable SIDValue
+}
